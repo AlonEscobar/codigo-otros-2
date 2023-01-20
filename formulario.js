29aca8379b1f4fc7319106089512cb0e1a866c10
Alon <alonzso07@gmail.com>
2023-01-20 11:40:42 -0600

    se realizo el programa

--- formulario.js
+++ formulario.js
@@ -1,18 +1,16 @@
-var formulario = document.querySelector("#form")
-
-formulario.onsubmit = function(e) {
-
-  e.prevent();
+let formulario = document.querySelector("#form");
+formulario.onsubmit = function() {
+  e.preventDefault();
   
-  var n = formulario.elements[0]
-  var e = formulario.elements[1]
-  var na = formulario.elements[2]
+  let n = document.getElementById("name");
+  let age = document.getElementById("age");
+  let na = document.getElementById("nationality");
 
-  var nombre = n.value
-  var edad = e.value
+  let nombre = n.value
+  let edad = age.value
 
-  var i = na.selectedIndex
-  var nacionalidad = na.options[i].value
+  let i = na.selectedIndex
+  let nacionalidad = na.options[i].value
   console.log(nombre, edad)
   console.log(nacionalidad)
 
@@ -20,62 +18,63 @@ formulario.onsubmit = function(e) {
     n.classList.add("error")
   }
   if (edad < 18 || edad > 120) {
-    e.classList.add("error")
+    age.classList.add("error")
   }
 
-if (nombre.length > 0 
-  && (edad > 18 
-    && edad < 120) ) {
-  agregarInvitado(nombre, edad, nacionalidad)
-  }
+  if (nombre.length > 0 
+    && (edad > 18 
+      && edad < 120) ) {
+    agregarInvitado(nombre, edad, nacionalidad)
+    }
 }
 
-var botonBorrar = document.createElement("button")
+let botonBorrar = document.createElement("button")
 botonBorrar.textContent = "Eliminar invitado"
 botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
+let corteLinea = document.createElement("br")
 document.body.appendChild(corteLinea)
 document.body.appendChild(botonBorrar);
 
 function agregarInvitado(nombre, edad, nacionalidad) {
+  
+  switch (nacionalidad) {
+    case "ar":
+      nacionalidad = "Argentina"
+      break;
+    case "mx":
+      nacionalidad = "Mexicana"
+      break;
+    case "vnzl":
+      nacionalidad = "Venezolana"
+      break;
+    case "per":
+      nacionalidad = "Peruana"
+      break;
+    default:
+      break;
+  }
 
-  if (nacionalidad === "ar") {
-    nacionalidad = "Argentina"
-  }
-  else if (nacionalidad === "mx") {
-    nacionalidad = "Mexicana"
-  }
-  else if (nacionalidad === "vnzl") {
-    nacionalidad = "Venezolana"
-  }
-  else if (nacionalidad === "per") {
-    nacionalidad = "Peruana"
-  }
+let lista = document.getElementById("lista-de-invitados")
 
-var lista = document.getElementById("lista-de-invitados")
-
-var elementoLista = document.createElement("div")
-elementoLista.classList.added("elemento-lista")
+let elementoLista = document.createElement("div")
+elementoLista.classList.add("elemento-lista")
 lista.appendChild(elementoLista)
 
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
-spanNombre.textContent = "Nombre: "
-inputNombre.value = nombre 
-elementoLista.appendChild(spanNombre)
-elementoLista.appendChild(inputNombre)
+let espacio = document.createElement("br")
 elementoLista.appendChild(espacio)
 
 function crearElemento(descripcion, valor) {
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
-spanNombre.textContent = descripcion + ": "
-inputNombre.value = valor 
-elementoLista.appendChild(spanNombre)
-elementoLista.appendChild(inputNombre)
-elementoLista.appendChild(espacio)
+  let fila = document.createElement("tr");
+  let columnaNombre = document.createElement("td");
+  let columnaEdad = document.createElement("td");
+  let columnaNacionalidad = document.createElement("td");
+  columnaNombre.textContent = descripcion + ": " + valor;
+  columnaEdad.textContent = edad;
+  columnaNacionalidad.textContent = nacionalidad;
+fila.appendChild(columnaNombre);
+fila.appendChild(columnaEdad);
+fila.appendChild(columnaNacionalidad);
+document.querySelector("#lista-de-invitados tbody").appendChild(fila);
 }
 
 crearElemento("Nombre", nombre)
@@ -83,10 +82,10 @@ crearElemento("Edad", edad)
 crearElemento("Nacionalidad", nacionalidad)
 
 
-var botonBorrar = document.createElement("button")
+let botonBorrar = document.createElement("button")
 botonBorrar.textContent = "Eliminar invitado"
 botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
+let corteLinea = document.createElement("br")
 elementoLista.appendChild(corteLinea)
 elementoLista.appendChild(botonBorrar);
 
